83c76dda703c9eb5747133fec6842ee404e94f63
wrtlpms <wurotalepharmacy@gmail.com>
2025-05-27 00:46:44 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-  const menu = document.getElementById('mobile-menu');
-  const navList = document.querySelector('.nav-list');
-  menu.addEventListener('click', () => {
-    navList.classList.toggle('active');
-  });
-});
